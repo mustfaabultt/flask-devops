d26782558f747b69eb7c79d01e5ecae17ace90f3
Mustafa <mustfaabultt@gmail.com>
2026-05-04 22:30:51 +0300

    robotu uyandir

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Merhaba! 🚀</h1><p>Bu uygulama Docker, Ansible ve GitHub Actions ile Alibaba Cloud'a otomatik olarak deploy edildi.</p>"
+    return "<h1>Merhaba! 🚀🚀🚀</h1><p>Bu uygulama Docker, Ansible ve GitHub Actions ile Alibaba Cloud'a otomatik olarak deploy edildi.</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
